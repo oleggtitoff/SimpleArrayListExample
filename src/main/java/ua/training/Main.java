c9db9432c429fa74a10533110df4cb8309ad5ce3
Oleg Titov <oleggtitoff@gmail.com>
2019-12-17 10:51:25 +0200

    Added code from video.

---
+++ src/main/java/ua/training/Main.java
@@ -0,0 +1,30 @@
+package ua.training;
+
+import ua.training.model.entity.Simple;
+import ua.training.model.entity.SimpleArray;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Simple<String> strings = new SimpleArray<>();
+
+        strings.add("first");
+        strings.add("second");
+        strings.add("three");
+
+        print(strings);
+
+        strings.update(1, "updated");
+        print(strings);
+
+        strings.delete(0);
+        print(strings);
+    }
+
+    private static void print(Simple<String> strings) {
+        for (String s : strings) {
+            System.out.println(s);
+        }
+    }
+
+}
---
+++ src/main/java/ua/training/model/entity/ArrayIterator.java
@@ -0,0 +1,23 @@
+package ua.training.model.entity;
+
+import java.util.Iterator;
+
+public class ArrayIterator<E> implements Iterator<E> {
+    private int index = 0;
+    private E[] values;
+
+    ArrayIterator(E[] values) {
+        this.values = values;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < values.length;
+    }
+
+    @Override
+    public E next() {
+        return values[index++];
+    }
+
+}
